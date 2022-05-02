76b43202d236b56a75d9195cfcd14b0cf41a2702
Angel Ivan Martinez Garrido <mgangelivan@gmail.com>
2022-05-01 22:36:22 -0500

    Actualización del método render para react 18

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import './node_modules/normalize.css/normalize.css';
 import './public/styles.scss';
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot} from 'react-dom/client';
 import App from './src/components/App/App.jsx';
 
-render(<App />, document.getElementById('root'));
+const container = document.querySelector("#root");
+const root = createRoot(container);
+root.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+)
