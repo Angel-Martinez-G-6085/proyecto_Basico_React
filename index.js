1421ec7bc51ac4dccc9f307133e9c9ab78cfe954
Angel Ivan Martinez Garrido <mgangelivan@gmail.com>
2022-05-01 22:51:31 -0500

    plantilla de react actualizada

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import './node_modules/normalize.css/normalize.css';
 import './public/styles.scss';
 import React from 'react';
 import { createRoot} from 'react-dom/client';
-import App from './src/components/App/App.jsx';
+import App from './src/App.jsx';
 
 const container = document.querySelector("#root");
 const root = createRoot(container);
